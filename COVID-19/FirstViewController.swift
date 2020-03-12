331de37fd257a03ed95bb248a7d834b639e837f1
Edward Chen <cyyedward@hotmail.com>
2020-03-12 13:10:38 -0700

    Initial Commit

---
+++ COVID-19/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  COVID-19
+//
+//  Created by Edward Chen on 2020-03-12.
+//  Copyright © 2020 Edward Chen. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ COVID-19/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  COVID-19
+//
+//  Created by Edward Chen on 2020-03-12.
+//  Copyright © 2020 Edward Chen. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
