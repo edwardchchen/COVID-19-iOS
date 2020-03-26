6032d6ce1a6eff791428abf75d4b11c2c3ebf36e
Edward Chen <cyyedward@hotmail.com>
2020-03-25 18:15:39 -0700

    tips scene completed

--- COVID-19/SecondViewController.swift
+++ COVID-19/SecondViewController.swift
@@ -25,13 +25,13 @@ class SecondViewController: UIViewController {
         
     }
     func setUp(){
-        howVirusSpreadsTextView.text="1. Respiratory droplets generated when you cough or sneeze\n2. Close, prolonged personal contact, such as touching or shaking hands\n3. Touching something with the virus on it, then touching your mouth, nose or eyes before washing your hands"
+        howVirusSpreadsTextView.text="1. Respiratory droplets generated when you cough or sneeze\n2. Close, prolonged personal contact, such as touching or shaking hands.\n3. Touching something with the virus on it, then touching your mouth, nose or eyes before washing your hands."
         
-        selfIsoLateTextView.text = "Self-monitor\nMonitor yourself for 14 days for symptoms of respiratory illness, such as cough, fever and difficulty breathing avoid crowded places and increase your personal space from others whenever possible."
+        selfIsoLateTextView.text = "Monitor yourself for 14 days for symptoms of respiratory illness, such as cough, fever and difficulty breathing avoid crowded places and increase your personal space from others whenever possible."
         
-        socialDIstaceTextView.text="Social Distancing\n1. Avoiding crowded places and non-essential gatherings\n2. Avoiding common greetings, such as handshakes\n3. Limiting contact with people at higher risk like older adults and those in poor health\n4. Keeping a distance of at least 2 arms-length (approximately 2 metres) from others"
+        socialDIstaceTextView.text="1. Avoiding crowded places and non-essential gatherings.\n2. Avoiding common greetings, such as handshakes.\n3. Limiting contact with people at higher risk like older adults and those in poor health.\n4. Keeping a distance of at least 2 arms-length (approximately 2 metres) from others."
         
-        hygieneTextView.text="Hygiene\n1. Wash your hands often with soap and water for at least 20 seconds, especially after using the washroom and when preparing food\n2. Cough or sneeze into a tissue or the bend of your arm, not your hand"
+        hygieneTextView.text="1. Wash your hands often with soap and water for at least 20 seconds, especially after using the washroom and when preparing food.\n2. Cough or sneeze into a tissue or the bend of your arm, not your hand.\nSource:\nhttps://www.canada.ca/en/public-health/services/diseases/2019-novel-coronavirus-infection/prevention-risks.html?topic=tilelink#h"
 
     }
     func adjustUITextViewHeight(arg : UITextView)
